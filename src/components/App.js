eb3a9ec47728b9387c9b432fc9bd192f367cbcc4
Nestor Masinde <nestormasinde@gmail.com>
2024-02-04 16:18:46 +0300

    Data_Fetching_Lab Successful!

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, {useState, useEffect} from 'react'
+
+const App = () => {
+    
+        const [dogImage, setdogImage] = useState([]);
+        const [isLoaded, setIsLoaded] = useState(false);
+      
+        useEffect(() => {
+          fetch("https://dog.ceo/api/breeds/image/random")
+            .then((response) => response.json())
+            .then((data) => {
+            console.log(data)
+              setdogImage(data.message);
+              setIsLoaded(true);
+            });
+        }, []);
+      
+        // if the data hasn't been loaded, show a loading indicator
+        if (!isLoaded) return <h3>Loading...</h3>;
+      
+  return (
+    <div>
+      <img src={dogImage} alt='A Random Dog' />
+    </div>
+  )
+}
+
+export default App
